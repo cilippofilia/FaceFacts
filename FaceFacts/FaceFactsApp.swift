2576091c92a41a7bdc284859f55556bf417a430a
Filippo Cilia <filippocarlocilia@gmail.com>
2024-01-27 09:18:38 +0000

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Filippo Cilia on 27/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
